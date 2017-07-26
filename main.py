a0eece5d5e66c7b004ac7fba19db891eecfd1bf7
chenlb <chenlb@polarwin.cn>
2017-07-26 08:43:49 +0800

    server can be visited on other machine

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def ask():
 	        return jsonify({'status':'OK','answer':bot_response})
 
 if __name__ == "__main__":
-    app.run(debug=True,port = 7890)
+    app.run(debug=True,host = '0.0.0.0',port = 7890)
